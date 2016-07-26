038ed87fa0e20b252ecd1683006e4af206db02ed
Swarn <Swarn@DESKTOP-H976MNN>
2016-07-26 14:30:44 +1200

    added new room

--- hotel/src/hotel/MainClass.java
+++ hotel/src/hotel/MainClass.java
@@ -5,6 +5,9 @@ public class MainClass {
 	public static void main(String[] args) {
 		Room room1 = new Room("Suite", 120, 2, true);
 		room1.display();
+		
+		Room room2 = new Room("Single", 90, 5, true);
+		room2.display();
 	}
 
 }
